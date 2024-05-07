71a60ff12c6b5d31d208103dd2528f5934e30ccd
CalinCretu <calincretu1412@gmail.com>
2024-05-07 17:30:57 +0200

    ADD - debugging of AddNewGame(), added SearchGameById()

--- adonet-db-videogame/Program.cs
+++ adonet-db-videogame/Program.cs
@@ -4,7 +4,64 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Videogame!");
+            while (true)
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Add a new videogame");
+                Console.WriteLine("2. Search a videogame by Id");
+                Console.Write("Scelta: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddNewGame();
+                        break;
+                    case "2":
+                        SearchGameById();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Try again!");
+                        break;
+                }
+            }
+        }
+
+        static void AddNewGame()
+        {
+            Console.WriteLine("Add a new game to the library:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Overview: ");
+            string overview = Console.ReadLine();
+
+            VideogameManager.InsertVideogame(name, overview);
+            Console.WriteLine("New videogame added to library!");
+        }
+        static void SearchGameById()
+        {
+            Console.Write("Search a videogame by its ID: ");
+            string inputId = Console.ReadLine();
+            int id;
+
+            if (int.TryParse(inputId, out id))
+            {
+                Videogame game = VideogameManager.GetVideogameById(id);
+                if (game != null)
+                {
+                    Console.WriteLine($"A videogame has been found: {game.Name}");
+                }
+                else
+                {
+                    Console.WriteLine("No videogames has been found.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid ID.");
+            }
         }
     }
 }
--- adonet-db-videogame/Videogame.cs
+++ adonet-db-videogame/Videogame.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace adonet_db_videogame
 {
@@ -14,10 +15,10 @@ namespace adonet_db_videogame
         public string Overview { get; set; }
         public DateTime ReleaseDate { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set;}
-        public int SoftwareHouseId { get; set; } // chiave esterna tabella software_houses
+        public DateTime UpdatedAt { get; set; }
+        public long SoftwareHouseId { get; set; } // chiave esterna tabella software_houses
 
-        public Videogame(string name, string overview, DateTime releaseDate, DateTime createdAt, DateTime updatedAt, int softwareHouseId)
+        public Videogame(string name, string overview, DateTime releaseDate, DateTime createdAt, DateTime updatedAt, long softwareHouseId)
         {
             Name = name;
             Overview = overview;
@@ -30,36 +31,57 @@ namespace adonet_db_videogame
 
     public static class VideogameManager
     {
-        static string connectionString = "Data Source=localhost;Initial Catalog=adonet-db-videogame;Integrated Security=True;Trust Server Certificate=True";
-        static void InsertVideogame(string name, string overview, DateTime release_date, DateTime created_at, DateTime updated_at, int software_house_id)
+        static string connectionString = "Data Source=localhost;Initial Catalog=adonet-db-videogame;Integrated Security=True";
+        public static void InsertVideogame(string name, string overview)
         {
-            Videogame NewVideogame = new Videogame(name, overview, release_date, created_at, updated_at, software_house_id);
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
 
-            string query = "INSERT INTO videogames (name, overview, release_date, created_at, updated_at, software_house_id) VALUES (@name, @overview, @release_date, @created_at, @updated_at, @software_house_id)";
-
-            SqlConnection sqlConnection = new SqlConnection(connectionString);
             try
             {
                 sqlConnection.Open();
 
-                SqlCommand cmd = new SqlCommand(query, sqlConnection);
-                cmd.Parameters.Add(new SqlParameter("@name", name));
-                cmd.Parameters.Add(new SqlParameter("@overview", overview));
-                cmd.Parameters.Add(new SqlParameter("@release_date", release_date));
-                cmd.Parameters.Add(new SqlParameter("@created_at", created_at));
-                cmd.Parameters.Add(new SqlParameter("@updated_at", updated_at));
-                cmd.Parameters.Add(new SqlParameter("@software_house_id", software_house_id));
+                string query = "INSERT INTO videogames (name, overview, release_date, created_at, updated_at, software_house_id) VALUES (@name, @overview, @release_date, @created_at, @updated_at, @software_house_id)";
 
-                cmd.ExecuteNonQuery();
+                using SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                {
+                    cmd.Parameters.Add(new SqlParameter("@name", name));
+                    cmd.Parameters.Add(new SqlParameter("@overview", overview));
+                    cmd.Parameters.Add(new SqlParameter("@release_date", "1985-01-01"));
+                    cmd.Parameters.Add(new SqlParameter("@created_at", "1985-01-01"));
+                    cmd.Parameters.Add(new SqlParameter("@updated_at", "2002-01-01"));
+                    cmd.Parameters.Add(new SqlParameter("@software_house_id", "1"));
+
+                    int affectedRows = cmd.ExecuteNonQuery();
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
-            finally
+        }
+        public static Videogame GetVideogameById(int id)
+        {
+            string query = "SELECT * FROM videogames WHERE id = @id";
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
-                sqlConnection.Close();
+                sqlConnection.Open();
+                SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    return new Videogame(
+                        reader["name"].ToString(),
+                        reader["overview"].ToString(),
+                        Convert.ToDateTime(reader["release_date"]),
+                        Convert.ToDateTime(reader["created_at"]),
+                        Convert.ToDateTime(reader["updated_at"]),
+                        (long)reader["software_house_id"]
+                    );
+                }
             }
+            return null;
         }
     }
 }
