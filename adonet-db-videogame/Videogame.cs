5c8a55124a45bb613f59b1e9cb0e9dbc5e7ea46f
CalinCretu <calincretu1412@gmail.com>
2024-05-07 15:06:27 +0200

    ADD - connessione a DB e funzione per inserire videogame

--- adonet-db-videogame/Videogame.cs
+++ adonet-db-videogame/Videogame.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace adonet_db_videogame
         public DateTime UpdatedAt { get; set;}
         public int SoftwareHouseId { get; set; } // chiave esterna tabella software_houses
 
-        public Videogame(int id, string name, string overview, DateTime releaseDate, DateTime createdAt, DateTime updatedAt, int softwareHouseId)
+        public Videogame(string name, string overview, DateTime releaseDate, DateTime createdAt, DateTime updatedAt, int softwareHouseId)
         {
             Id = id;
             Name = name;
@@ -30,10 +31,36 @@ namespace adonet_db_videogame
 
     public static class VideogameManager
     {
-        static void InsertVideogame( int id, string name, string overview, DateTime release_date, DateTime created_at, DateTime updated_at, int software_house_id)
+        static string connectionString = "Data Source=localhost;Initial Catalog=adonet-db-videogame;Integrated Security=True;Trust Server Certificate=True";
+        static void InsertVideogame(string name, string overview, DateTime release_date, DateTime created_at, DateTime updated_at, int software_house_id)
         {
-            Videogame NewVideogame = new Videogame(id, name, overview, release_date, created_at, updated_at, software_house_id);
+            Videogame NewVideogame = new Videogame(name, overview, release_date, created_at, updated_at, software_house_id);
 
+            string query = "INSERT INTO videogames (name, overview, release_date, created_at, updated_at, software_house_id) VALUES (@name, @overview, @release_date, @created_at, @updated_at, @software_house_id)";
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            try
+            {
+                sqlConnection.Open();
+
+                SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                cmd.Parameters.Add(new SqlParameter("@name", name));
+                cmd.Parameters.Add(new SqlParameter("@overview", overview));
+                cmd.Parameters.Add(new SqlParameter("@release_date", release_date));
+                cmd.Parameters.Add(new SqlParameter("@created_at", created_at));
+                cmd.Parameters.Add(new SqlParameter("@updated_at", updated_at));
+                cmd.Parameters.Add(new SqlParameter("@software_house_id", software_house_id));
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
         }
     }
 }
